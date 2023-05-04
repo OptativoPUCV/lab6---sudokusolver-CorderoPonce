b021f21f65da50036ca674f766ea9d314eca9d57
Replit user <>
2023-05-04 23:12:50 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -54,8 +54,8 @@ List* get_adj_nodes(Node* n){
   Node *new = n;
   int num = 1;
 
-  for (int i = 1 ; i < 10 ; i++){
-    for (int j = 1 ; j < 10 ; j++){
+  for (int i = 0 ; i < 9 ; i++){
+    for (int j = 0 ; j < 9 ; j++){
       
       new->sudo[i][j] = num;
       pushBack(list, new);
